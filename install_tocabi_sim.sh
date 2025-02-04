21f241b09d3160806d26630b5dcf4293ed1dbc26
kwlee365 <kwlee365@snu.ac.kr>
2025-02-04 12:54:55 +0900

    Add TOCABI Simulation Install Script

---
+++ install_tocabi_sim.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# Apache License 2.0
+# Copyright (c) 2024, DYROS Robotics
+
+# TOCABI Simulation Installation Script for Ubuntu 24.04
+# This script installs Distrobox, creates an Ubuntu 20.04 container, and sets up ROS Noetic & TOCABI Simulation.
+
+set -e  # Stop on error
+
+# Step 1: Install Distrobox and Podman
+echo "[Step 1] Installing Distrobox and Podman..."
+sudo apt update -y
+sudo apt install -y curl podman
+curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
+
+# Step 2: Create Ubuntu 20.04 Container
+echo "[Step 2] Creating Ubuntu 20.04 container using Distrobox..."
+mkdir -p ~/ubuntu-20-04  # Create a directory for the container home
+
+distrobox create --image ubuntu:20.04 \
+                 --name ubuntu-20-04 \
+                 --hostname ubuntu-20-04 \
+                 --home ~/ubuntu-20-04 \
+                 --nvidia
+
+echo "[Step 3] Entering Ubuntu 20.04 container and installing ROS Noetic..."
+distrobox enter ubuntu-20-04 -- sh -c "
+    set -e
+    sudo apt update -y
+    sudo sh -c 'echo \"deb http://packages.ros.org/ros/ubuntu focal main\" > /etc/apt/sources.list.d/ros-latest.list'
+    curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+    sudo apt update -y
+    sudo apt install -y ros-noetic-desktop-full python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential git
+    sudo rosdep init
+    rosdep update
+    sudo apt install -y python3-catkin-tools
+    echo \"source /opt/ros/noetic/setup.bash\" >> ~/.bashrc
+    source ~/.bashrc
+"
+
+echo "[Step 4] Installing TOCABI Simulation in the container..."
+distrobox enter ubuntu-20-04 -- sh -c "
+    set -e
+    sudo apt update -y
+    sudo apt install -y cmake g++ libeigen3-dev libyaml-cpp-dev libjsoncpp-dev qtbase5-private-dev libqt5x11extras5*
+    mkdir -p ~/catkin_ws/src
+    cd ~/catkin_ws/src
+    git clone --recurse-submodules https://github.com/saga0619/dyros_tocabi_v2
+    git clone https://github.com/saga0619/tocabi_cc
+    git clone https://github.com/saga0619/tocabi_avatar
+    git clone https://github.com/saga0619/tocabi_gui
+    git clone https://github.com/saga0619/mujoco_ros_sim
+    
+    # Install required libraries manually instead of install_prereq.sh
+    mkdir -p ~/lib && cd ~/lib
+    
+    # Install RBDL
+    git clone https://github.com/saga0619/rbdl-orb
+    cd rbdl-orb
+    mkdir build && cd build
+    cmake ..
+    make all
+    echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib' >> ~/.bashrc
+    sudo ldconfig
+    sudo make install
+    
+    # Install qpOASES
+    cd ~/lib
+    git clone https://github.com/saga0619/qpoases
+    cd qpoases
+    mkdir build && cd build
+    cmake ..
+    make all
+    sudo make install
+    
+    # Install MSCL
+    cd ~/lib
+    wget https://github.com/LORD-MicroStrain/MSCL/releases/download/v52.2.1/c++-mscl_52.2.1_amd64.deb
+    sudo dpkg -i c++-mscl_52.2.1_amd64.deb
+    
+    cd ~/catkin_ws
+    source /opt/ros/noetic/setup.bash
+    catkin build
+    echo \"source ~/catkin_ws/devel/setup.bash\" >> ~/.bashrc
+    source ~/.bashrc
+"
+
+echo "[Installation Complete!] You can now enter the container using: distrobox enter ubuntu-20-04"
+
